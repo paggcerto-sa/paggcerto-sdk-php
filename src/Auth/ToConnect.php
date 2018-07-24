82e3edaa74cf04d6275b0c23175b55913863907e
Erick Antunes <erick.ants@gmail.com>
2018-07-24 17:25:06 -0300

    Arvores de dados adicionada, Holder account iniciado

---
+++ src/Auth/ToConnect.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * User: erick.antunes
+ * Date: 24/07/2018
+ * Time: 11:49
+ */
+
+namespace Paggcerto\Auth;
+
+use JsonSerializable;
+use Paggcerto\Contracts\Authentication;
+use Paggcerto\Paggcerto;
+use Requests_Session;
+
+class ToConnect implements JsonSerializable
+{
+    private $oauth_token = "api/v2/%s/signin";
+    private $paggcertoAuthentication;
+    private $applicationNumber;
+    private $endpoint;
+    private $session;
+
+    public function __construct(Authentication $paggcertoAuthentication, $endpoint = Paggcerto::ACCOUNT_ENDPOINT_SANDBOX,
+                                $applicationNumber)
+    {
+        $this->paggcertoAuthentication = $paggcertoAuthentication;
+        $this->applicationNumber = $applicationNumber;
+        $this->endpoint = sprintf($this->oauth_token, $this->applicationNumber);
+    }
+
+    public function createNewSession($timeout = 30.0, $connect_timeout = 30.0)
+    {
+        $user_agent = sprintf('%s/%s (+https://github.com/paggcerto-sa/paggcerto-sdk-php/)', Paggcerto::CLIENT,
+            Paggcerto::CLIENT_VERSION);
+        $sess = new Requests_Session($this->endpoint);
+        $sess->options['auth'] = $this->paggcertoAuthentication;
+        $sess->options['timeout'] = $timeout;
+        $sess->options['connect_timeout'] = $connect_timeout;
+        $sess->options['useragent'] = $user_agent;
+        $this->session = $sess;
+
+        return $sess;
+    }
+
+    public function getAuthUrl($endpoint = null)
+    {
+        if ($endpoint !== null) {
+            $this->endpoint = $endpoint;
+        }
+        return $endpoint;
+    }
+
+    public function authorize()
+    {
+        $urlPath = $this->endpoint . $this->oauth_token;
+        $headers = ["Content-Type" => "application/json"];
+        $body = [
+
+        ];
+    }
+
+    function jsonSerialize()
+    {
+        // TODO: Implement jsonSerialize() method.
+    }
+}
---
+++ src/Contracts/Authentication.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Paggcerto\Contracts;
+
+use Requests_Auth;
+
+interface Authentication extends Requests_Auth
+{
+}
